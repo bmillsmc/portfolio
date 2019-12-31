a2af532a3758b1013452fd025edbc965b58f8a55
Ben Miller <benbmiller123@gmail.com>
2019-12-31 14:23:54 -0500

    changed app to a class

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+// import Sidebar from "./components/Sidebar/Sidebar";
 
-function App() {
-  return <h1>Hello</h1>;
+//TODO: pass state for sidebar being open or shut with a mouseover event
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <aside>{/* <Sidebar /> */}</aside>
+        <main>
+          <h1>temp</h1>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
