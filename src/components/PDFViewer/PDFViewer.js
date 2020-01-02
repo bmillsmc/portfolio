cf0d46b44a5bce5595b58013616f8690aeb09c05
Ben Miller <benbmiller123@gmail.com>
2020-01-02 15:36:21 -0500

    connected pdfviewer to pdfbackend

--- src/components/PDFViewer/PDFViewer.js
+++ src/components/PDFViewer/PDFViewer.js
@@ -8,8 +8,14 @@ class PDFViewer extends Component {
     this.backend = new props.backend();
   }
 
+  componentDidMount() {
+    const { src } = this.props;
+    const element = this.viewerRef.current;
+    this.backend.init(src, element);
+  }
+
   render() {
-    return <div id="viewer">Hello World</div>;
+    return <div ref={this.viewerRef} id="viewer"></div>;
   }
 }
 
